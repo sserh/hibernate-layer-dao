9ab089eacda996971451ab0b15c1bfd7ead587ab
sserh <serhios@bk.ru>
2024-07-18 15:44:16 +0700

    - Refactoring

--- src/main/java/ru/raccoon/hibernatelayerdao/model/Person.java
+++ src/main/java/ru/raccoon/hibernatelayerdao/model/Person.java
@@ -1,12 +1,12 @@
 package ru.raccoon.hibernatelayerdao.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Optional;
 
 @Data
 @Entity
