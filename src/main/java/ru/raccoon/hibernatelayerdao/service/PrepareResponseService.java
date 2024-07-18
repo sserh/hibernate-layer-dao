ca24586e986af23f3213d2ac1d65d8e56d5cc561
sserh <serhios@bk.ru>
2024-07-18 17:18:29 +0700

    - personRepository is private and final now

--- src/main/java/ru/raccoon/hibernatelayerdao/service/PrepareResponseService.java
+++ src/main/java/ru/raccoon/hibernatelayerdao/service/PrepareResponseService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Service
 public class PrepareResponseService {
 
-    CustomizedPersonsCrudRepository personRepository;
+    private final CustomizedPersonsCrudRepository personRepository;
 
     public PrepareResponseService(CustomizedPersonsCrudRepository personRepository) {
         this.personRepository = personRepository;
